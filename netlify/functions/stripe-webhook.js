800eaff4d04d7bb31d307d46bf1d86d9033d6b23
Little Big Hope <office@west-side-productions.at>
2024-12-10 23:23:48 +0100

    asdf

--- netlify/functions/stripe-webhook.js
+++ netlify/functions/stripe-webhook.js
@@ -19,9 +19,11 @@ async function addPlanToMember(memberId, planId) {
             planId: planId
         };
         const headers = {
-            "X-API-KEY": process.env.MEMBERSTACK_SECRET_KEY
+            "Authorization": process.env.MEMBERSTACK_SECRET_KEY,
+            "Content-Type": "application/json"
         };
 
+        console.log('Making request to Memberstack:', { url, planId, memberId });
         const response = await axios.post(url, data, { headers });
         console.log(`Successfully added plan ${planId} to member ${memberId}`, response.data);
     } catch (error) {
